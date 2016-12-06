9663979c90e6b39e61ae8a73bc23466531755d27
Brian Xu <bwxu@mit.edu>
2016-12-05 22:21:33 -0800

    refactored code to hopefully make it more runnable.

--- test.py
+++ test.py
@@ -40,49 +40,71 @@ def batch_norm(x, n_out, phase_train):
         normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-3)
     return normed
 
-# training_data = matrix with dimension [num_images, 256, 256, 1]
-
 
 # https://github.com/richzhang/colorization/blob/master/models/colorization_train_val_v2.prototxt
 
-phase_train = tf.placeholder(tf.bool, name='phase_train')
+def cnn_model(x):
 
-Wconv1_1 = weight_variable([3, 3, 1, 64])
-bconv1_1 = bias_variable([64])
+    phase_train = tf.placeholder(tf.bool, name='phase_train')
 
-Rconv1_1 = tf.nn.relu(conv(training_data, Wconv1_1, 1) + bconv1_1)
+    Wconv1_1 = weight_variable([3, 3, 1, 64])
+    bconv1_1 = bias_variable([64])
 
-Wconv1_2 = weight_variable([3, 3, 64, 64])
-bconv1_2 = bias_variable([64])
+    Rconv1_1 = tf.nn.relu(conv(x, Wconv1_1, 1) + bconv1_1)
 
-Rconv1_2 = tf.nn.relu(conv(Rconv1_1, Wconv1_2, 2) + bconv1_2)
+    Wconv1_2 = weight_variable([3, 3, 64, 64])
+    bconv1_2 = bias_variable([64])
 
-Rnorm1 = batch_norm(Rconv1_2, 64, phase_train)
+    Rconv1_2 = tf.nn.relu(conv(Rconv1_1, Wconv1_2, 2) + bconv1_2)
 
-
+    Rnorm1 = batch_norm(Rconv1_2, 64, phase_train)
+    
+    Wconv2_1 = weight_variable([3, 3, 1, 128])
+    bconv2_1 = bias_variable([128])
+    
+    Rconv2_1 = tf.nn.relu(conv(Rnorm1, Wconv2_1, 1) + bconv2_1)
+    
+    Wconv2_2 = weight_variable([3, 3, 1, 128])
+    bconv2_2 = bias_variable([128])
+    
+    Rconv2_2 = tf.nn.relu(conv(Rnorm1, Wconv2_1, 2) + bconv2_1)
+    
+    # TODO: write in the rest of the cnn
+    
+    return output
 
 
 # training and evaluating, but not for our case
 # https://www.tensorflow.org/versions/r0.12/tutorials/mnist/pros/index.html#convolution-and-pooling
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-sess.run(tf.global_variables_initializer())
-for i in range(20000):
-  batch = mnist.train.next_batch(50)
-  if i%100 == 0:
-    train_accuracy = accuracy.eval(feed_dict={
-        x:batch[0], y_: batch[1], keep_prob: 1.0})
-    print("step %d, training accuracy %g"%(i, train_accuracy))
-  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-print("test accuracy %g"%accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+def train_cnn():
+    prediction = neural_network_model(x)
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction, y_))
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    
+    correct_prediction = tf.equal(tf.argmax(prediction,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for i in range(20000):
+            batch = mnist.train.next_batch(50)
+            
+            if i%100 == 0:
+                train_accuracy = accuracy.eval(feed_dict={
+                    x:batch[0], y_: batch[1], keep_prob: 1.0})
+            
+            print("step %d, training accuracy %g"%(i, train_accuracy))
 
+            train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
+        print("test accuracy %g"%accuracy.eval(feed_dict={
+            x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+        
+if __name__ == '__main__':
 
-
-
-
+    # training_data = matrix with dimension [num_images, 256, 256, 1]
+    x = tf.placeholder('float', [None, 256*256])
+    y = tf.placeholder('float')
+    train_cnn(x)
 
